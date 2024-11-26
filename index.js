fc910a1a87738fc564e2fd5270685647e26d8d52
Brandon Bakus <brandonbakus@gmail.com>
2024-11-26 16:35:04 -0500

    This took me too long, it's shameful.

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+
+function introduction(Name1 = "Aki", Name2 = "Samip"){
+    
+    return `Hi, my name is ${Name1}.`
+    return`Hi, my name is ${Name2}.`
+}
+    
+
+
+
+function introductionWithLanguage(Name = "Aki", Language0 = "Ember.js", Name0 = "Samip", Language = "React"){
+   return `Hi, my name is ${Name} and I am learning to program in ${Language0}.`
+   return `Hi, my name is ${Name0} and I am learning to program in ${Language}.`
+}
+
+
+
+
+function introductionWithLanguageOptional(Name = "Gracie", Language = "JavaScript", Language2 = "Python"){
+    return `Hi, my name is ${Name} and I am learning to program in ${Language}.`
+    return `Hi, my name is ${Name} and I am learning to program in ${Language2}.`
+}
+
